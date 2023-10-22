4753fd3d5fbfb43234ead60ce2c28d607e11840a
Eric Chen <98416392+Erichen294@users.noreply.github.com>
2023-10-22 17:19:25 -0400

    Update server.js

--- skills/cluster-3/21/code/server.js
+++ skills/cluster-3/21/code/server.js
@@ -3,4 +3,4 @@ const http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-  }).listen(8080, '192.168.1.42');
+  }).listen(8082);
